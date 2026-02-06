8339e48b8a503ce08a90c02b42bd82b57588d4e3
commandblock2 <tony_hu@live.com>
2026-02-06 12:20:28 +0800

    chore: demo, with relative dir and more buttons

--- demo/src/main.ts
+++ demo/src/main.ts
@@ -1,24 +1,24 @@
-import './style.css';
-import { applyEffect } from 'fluent-reveal-effect';
+import "./style.css";
+import { applyEffect } from "../../src";
 
-applyEffect('.toolbar', {
-  lightColor: 'rgba(255,255,255,0.1)',
-  gradientSize: 500
+applyEffect(".toolbar", {
+  lightColor: "rgba(255,255,255,0.1)",
+  gradientSize: 500,
 });
 
-applyEffect('.toolbar > .btn', {
-  clickEffect: true
-});
-
-applyEffect('.effect-group-container', {
+applyEffect(".toolbar > .btn", {
   clickEffect: true,
-  lightColor: 'rgba(255,255,255,0.6)',
+});
+
+applyEffect(".effect-group-container", {
+  clickEffect: true,
+  lightColor: "rgba(255,255,255,0.6)",
   gradientSize: 80,
   isContainer: true,
   children: {
-    borderSelector: '.btn-border',
-    elementSelector: '.btn',
-    lightColor: 'rgba(255,255,255,0.3)',
-    gradientSize: 150
-  }
+    borderSelector: ".btn-border",
+    elementSelector: ".btn",
+    lightColor: "rgba(255,255,255,0.3)",
+    gradientSize: 150,
+  },
 });
